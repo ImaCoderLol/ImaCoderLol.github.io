e7119bbd09ee4035e57718268927796bbad1c83d
david-ortega <david-ortega@codermerlin.codermerlin.com>
2023-01-13 12:47:29 -0600

    Assignment Update for January

--- CS-II/Projects/Recursion/main.swift
+++ CS-II/Projects/Recursion/main.swift
@@ -1,9 +1,14 @@
-func f() {
-    g(3)
-}
-func g(_ n: Int) {
-    print(n)
-    g(n - 1)
+import Foundation
+
+func calc(_ n: Int) {
+    print("start")
+    if n > 2 {
+        print(n)
+        calc(n / 2)
+        print(n)
+    } else {
+        print("going down")
+    }
 }
 func countdown(_ n: Int) {
     print(n)
@@ -16,5 +21,27 @@ func countdown(_ n: Int) {
     }
 }
 
-countdown(5)
-print("End")
+func test(_ x: Int, _ y: Int) {
+    if x == 0 {
+        print(y)
+    } else {
+        print(x, y)
+        return test(x - 1, x + y)
+    }
+}
+
+func work(_ n: Int) -> Int {
+    print(n)
+    if n > 4 {
+        return work(n - 1) + work(n - 2) + work(n - 3)
+    } else {
+        return 0
+    }
+    try await Task.sleep(nanoseconds: 3_000_000_000)
+}
+
+calc(12)
+print("separation")
+
+test(7, 1)
+print(work(107))
